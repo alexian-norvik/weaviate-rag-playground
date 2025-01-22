33bf48e90bf2d93c188dc5edf56487525253aa5d
Norvik Alexian <norvik@ucraft.com>
2025-01-22 15:37:09 +0400

    did simple semantic search on temporary data

--- services/retriever.py
+++ services/retriever.py
@@ -12,8 +12,14 @@ data = json.loads(data_response.text)
 questions = client.collections.get("Question")
 
 # Insert objects in the collection
-with questions.batch.dynamic() as batch:
-    for d in data:
-        batch.add_object({"answer": d["Answer"], "question": d["Question"], "category": d["Category"]})
+# with questions.batch.dynamic() as batch:
+#     for d in data:
+#         batch.add_object({"answer": d["Answer"], "question": d["Question"], "category": d["Category"]})
+
+# Semantic search which is called `nearText` in Weaviate
+response = questions.query.near_text(query="biology", limit=2)
+
+for obj in response.objects:
+    print(json.dumps(obj.properties, indent=2))
 
 client.close()
