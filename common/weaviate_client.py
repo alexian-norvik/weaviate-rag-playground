48e8b9fb10a57b34fc7783677d77583edfc82782
Norvik Alexian <norvik@ucraft.com>
2025-01-22 15:34:57 +0400

    load the openai api key in the weaviate client

--- common/weaviate_client.py
+++ common/weaviate_client.py
@@ -1,19 +1,18 @@
 import weaviate
 from weaviate.classes.init import Auth
-from weaviate.classes.config import Configure
 
 import config
 
 # Connect to the cluster
 client = weaviate.connect_to_weaviate_cloud(
-    cluster_url=config.WCD_URL, auth_credentials=Auth.api_key(config.WCD_API_KEY)
+    cluster_url=config.WCD_URL,
+    auth_credentials=Auth.api_key(config.WCD_API_KEY),
+    headers={"X-OpenAI-Api-Key": config.OPENAI_API_KEY},
 )
 
 # Create collection
-questions = client.collections.create(
-    name="Question",
-    vectorizer_config=Configure.Vectorizer.text2vec_openai(),
-    generative_config=Configure.Generative.openai(),
-)
-
-client.close()
+# questions = client.collections.create(
+#     name="Question",
+#     vectorizer_config=Configure.Vectorizer.text2vec_openai(),
+#     generative_config=Configure.Generative.openai(),
+# )
