a4464dc328be7ebe50de6c096f1707580988b60b
Norvik Alexian <norvik@ucraft.com>
2025-01-22 15:07:39 +0400

    created collection

--- common/weaviate_client.py
+++ common/weaviate_client.py
@@ -1,10 +1,19 @@
 import weaviate
 from weaviate.classes.init import Auth
+from weaviate.classes.config import Configure
 
 import config
 
+# Connect to the cluster
 client = weaviate.connect_to_weaviate_cloud(
     cluster_url=config.WCD_URL, auth_credentials=Auth.api_key(config.WCD_API_KEY)
+)
+
+# Create collection
+questions = client.collections.create(
+    name="Question",
+    vectorizer_config=Configure.Vectorizer.text2vec_openai(),
+    generative_config=Configure.Generative.openai(),
 )
 
 client.close()
